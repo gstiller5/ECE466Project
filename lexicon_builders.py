1fdf6ef7addd58896e9d4f74535cdb13c0d212ef
Griffin Stiller <gstiller5@Griffins-MacBook-Pro.local>
2018-04-22 00:53:59 -0700

    added more interesting lexicons

---
+++ lexicon_builders.py
@@ -0,0 +1,64 @@
+from pandas import *
+
+def build_1850_lexicon():
+    file = open('1850.tsv', 'rb')
+    words = []
+    scores = []
+    for f in file:
+        word = str(f).split('\\t')[0][2:]
+        words.append(word)
+        score = str(f).split('\\t')[1]
+        scores.append(score)
+        # print(word + '    \t ' + score)
+    return words, scores
+
+def build_1900_lexicon():
+    file = open('1900.tsv', 'rb')
+    words = []
+    scores = []
+    for f in file:
+        word = str(f).split('\\t')[0][2:]
+        words.append(word)
+        score = str(f).split('\\t')[1]
+        scores.append(score)
+        # print(word + '    \t ' + score)
+    return words, scores
+
+def build_1950_lexicon():
+    file = open('1950.tsv', 'rb')
+    words = []
+    scores = []
+    for f in file:
+        word = str(f).split('\\t')[0][2:]
+        words.append(word)
+        score = str(f).split('\\t')[1]
+        scores.append(score)
+        # print(word + '    \t ' + score)
+    return words, scores
+
+def build_2000_lexicon():
+    file = open('2000.tsv', 'rb')
+    words = []
+    scores = []
+    for f in file:
+        word = str(f).split('\\t')[0][2:]
+        words.append(word)
+        score = str(f).split('\\t')[1]
+        scores.append(score)
+        # print(word + '    \t ' + score)
+    return words, scores
+
+def build_standard_lexicon():
+    words = []
+    scores = []
+    file = open('lexicon_easy.csv', 'rb')
+
+    for f in file:
+        new_word = str(f).split(',')
+        new_word[0] = new_word[0][2:]  # clearing out extra characters
+        words.append(new_word[0])
+        new_word[1] = new_word[1][:-3]
+        scores.append(new_word[1])
+    return words, scores
+
+
--- main.py
+++ main.py
@@ -1,49 +1,54 @@
 import matplotlib.pyplot as plt
 from read_tweets import *
+from lexicon_builders import *
+import re
 
-file = open("lexicon_easy.csv", "rb")
-words = []
-words_no_sent = []
 pos = 0
 neg = 0
 neu = 0
-provocative = 0
+lex_words = 0
 
-for f in file:
-    new_word = str(f).split(',')
-    new_word[0] = new_word[0][2:] # clearing out extra characters
-    words_no_sent.append(new_word[0])
-    new_word[1] = new_word[1][:-3]
-    words.append(new_word)
+#set up the lexicon
+words = []
+scores = []
+words, scores = build_standard_lexicon()
+# words, scores = build_1850_lexicon()
+# words, scores = build_1900_lexicon()
+# words, scores = build_1950_lexicon()
+# words, scores = build_2000_lexicon()
 
+#set up the tweet list
 tweets = []
 # tweets = read_tweets_elon()
 tweets = read_tweets_donald()
-# example tweets
-tweets.append(["Griffin is the best thing to happen to python coding ever"])
-tweets.append(["The party pigs are raw at soccer and will never be forgotten"])
-tweets.append(["I hate ASU"])
-tweets.append(["ASU is the worst school in the state of Arizona"])
-tweets.append(["Sean miller is the best coach in college basketball"])
-tweets.append(["I am glad that Rich Rodriguez was fired, he was not helping the program"])
-tweets.append(["Griffin is the man - abundant abundance absolve positive good great bad poor"])
-tweets.append(["this one has no_provocative_words"])
 
-for x in tweets:
+# example tweets
+# tweets.append(["Griffin is the best thing to happen to python coding ever"])
+# tweets.append(["The party pigs are raw at soccer and will never be forgotten"])
+# tweets.append(["I hate ASU"])
+# tweets.append(["ASU is the worst school in the state of Arizona"])
+# tweets.append(["Sean miller is the best coach in college basketball"])
+# tweets.append(["I am glad that Rich Rodriguez was fired, he was not helping the program"])
+# tweets.append(["Griffin is the man - abundant abundance absolve positive good great bad poor"])
+# tweets.append(["this one has no_provocative_words"])
+
+for x in tweets: #cycle through tweet list
+    x.append(0.0)
     x.append(0)
-    x.append(0)
-    for y in x[0].split(' '):
-        if y in words_no_sent:
+    for y in x[0].split(' '): # seperate the tweet by the word
+        # y = y.strip(['.', ',', '!', '?']) # strip off punctuation
+        y = re.sub('[(){}<>!?.,#]', '', y)
+        if y in words: # check if the word is in the lexicon
             x[2] += 1 # add the number of words that are possible to analyze
-            x[1] += int(words[words_no_sent.index(y)][1]) # add the sentiment of the word to the tweet's score
+            x[1] += float(scores[words.index(y)]) # add the sentiment of the word to the tweet's score
     if x[2] != 0:
-        provocative += 1
+        lex_words += 1
 
 # sort the tweets by score and print the list
 tweets.sort(key=lambda x: x[1])
 tweets.reverse()
 for x in tweets:
-    print('Words: ' + str(x[2]) + '\tScore: ' + str(x[1]) + '\tTweet: ' + x[0])
+    print('Words: ' + str(x[2]) + '\tScore: ' + '%.2f'%x[1] + '\tTweet: ' + x[0])
     if x[1] > 0:
         pos += 1
     elif x[1] == 0:
@@ -51,14 +56,15 @@ for x in tweets:
     else:
         neg += 1
 
+
+print()
+print('Number of tweets: ' + str(len(tweets)))
+print('Number of tweets with lexicon words: ' + str(lex_words))
+print('Percentage of tweets with provocative language: ' + '%.2f' % ((lex_words / len(tweets)) * 100) + '%')
+
+# plot some findings
 plt.bar(range(3), [pos, neu, neg])
 plt.xticks(range(3), ["Positive", "Neutral", "Negative"])
 plt.title("Tweet Sentiment Analysis")
 plt.ylabel("# of Tweets")
-
-print()
-print('Number of tweets: ' + str(len(tweets)))
-print('Number of tweets with provocative words: ' + str(provocative))
-print('Percentage of tweets with provocative language: ' + str((provocative/len(tweets))*100))
-
 plt.show()
